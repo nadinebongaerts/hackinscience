4eacaa4cb51e7e362fbd009f17073b188a1c0de2
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 10:34:21 +0200

    solution

---
+++ exercises/008/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:29:14 2015
+
+@author: xavierduportet
+"""
+# for Pyhton a string is the same as a list
+for i in 'Hello World !':
+    print(i)
