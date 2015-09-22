1190161df73a56a6e1bb343078a0f1777722beec
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 15:10:34 +0200

    solution

---
+++ exercises/019/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:49:11 2015
+
+@author: xavierduportet
+"""
+
+import sys
+import operator
+sys.argv
+print(operator.add(int(sys.argv[1]), int(sys.argv[2])))
