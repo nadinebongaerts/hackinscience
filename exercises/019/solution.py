b532ef3cf26a5fd3f89ec02265b82b73e5844cb9
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 15:18:45 +0200

    solution

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -8,4 +8,7 @@ Created on Tue Sep 22 14:49:11 2015
 import sys
 import operator
 sys.argv
-print(operator.add(int(sys.argv[1]), int(sys.argv[2])))
+if len(sys.argv == 2):
+    print(operator.add(int(sys.argv[1]), int(sys.argv[2])))
+else:
+    print('usage: python3 solution.py OP1 OP2')
