2e6336de7888ff9a9c1173883c73cd9f8e6b3add
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 20:28:48 +0200

    solution

--- exercises/040/solution.py
+++ exercises/040/solution.py
@@ -4,8 +4,8 @@ Created on Tue Sep 22 18:49:58 2015
 
 @author: xavierduportet
 """
-
-for i in range(0, 100):
+even = list()
+for i in range(0, 101):
     if(i % 2) == 0:
-        sum = i + i
-        print(sum)
+        even.append(i)
+print(sum(even))
