81bbcfed70d11a66d3dc581876ae361de45573cf
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 14:43:16 +0200

    solution

---
+++ exercises/015/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:24:04 2015
+
+@author: xavierduportet
+"""
+import operator
+a = operator.truediv(245850922, 78256779)
+b = operator.truediv((1 + 5**(0.5)), 2)
+list = [a, b]
+for i in list:
+    print(i)
