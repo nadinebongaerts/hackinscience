c61612296dfd5602432f40cb2c94e34817177164
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-22 14:10:53 +0200

    solution

---
+++ exercises/014/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:25:37 2015
+
+@author: xavierduportet
+"""
+
+import sys
+if len(sys.argv) >= 1:
+    print(sys.argv[1])
+else:
+    print('usethon3 solution.py PARAM')
