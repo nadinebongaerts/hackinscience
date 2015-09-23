7023f3687efa7a828098f87c119ff9f0383074f6
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-23 16:36:51 +0200

    solution

---
+++ exercises/060/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 16:29:45 2015
+
+@author: xavierduportet
+"""
+
+keywords = []
+abc = "abcdefghijklmnopqrstuvwxyz"
+abcd = list(abc)
+for x in abcd:
+    for y in abcd:
+            keywords.append(x+y)
+print(keywords)
