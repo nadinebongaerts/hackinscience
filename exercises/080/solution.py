1582787408a16f22a06f9aa551d0b818d58c8086
Xavier Duportet <xavierduportet@MacBook-Air-de-Xavier.local>
2015-09-24 10:33:43 +0200

    solution

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -5,14 +5,9 @@ Created on Wed Sep 23 18:04:33 2015
 @author: xavierduportet
 """
 
-result = []
+import itertools
 
-abc = "abcdefghijklmnopqrstuvwxyz"
-
-for x in abc:
-    for y in abc:
-        if x != y:
-            result.append(x+y)
+result = itertools.combinations('abcdefghijklmnopqrstuwvyz', 2)
 
 for i in result:
     print(i)
