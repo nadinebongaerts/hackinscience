64e623cd1ae6f120249a63129074bcb3919d6075
Xavier Duportet <xavierduportet@wifi-172-30-19-24.wifi.univ-paris5.fr>
2015-09-24 17:48:52 +0200

    solution

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -4,8 +4,15 @@ Created on Thu Sep 24 14:53:03 2015
 
 @author: xavierduportet
 """
-
 import operator
+
+
+def sort_a_list(l):
+    return sorted(l)
+
+
+def sort_by_mark(my_class):
+    return sorted(my_class)
 
 
 def sort_by_name(my_class):
